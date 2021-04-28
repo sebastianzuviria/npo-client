87ae748cf16458aa57cc5a97a4fab9733080014b
Bzch <francobaisch00@gmail.com>
2021-04-28 10:55:36 -0300

    created slider component with SwiperJS

---
+++ src/components/Slider/slider.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.scss";
+
+function slider() {
+  return (
+    <div>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        ...
+      </Swiper>
+    </div>
+  );
+}
+
+export default slider;
