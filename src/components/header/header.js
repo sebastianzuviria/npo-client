b2a379a19bc6ea1ea64bb208847f34e39f8eab4a
BrunoCereda9979 <bruno9979@gmail.com>
2021-04-28 15:29:26 -0300

    Added mock data for the navbar items to simulate they are comming from the public data endpoint.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import { React, useState } from 'react';
 
 // Styles
 import './header.styles.css';
 
 // Components
 import NavItem from '../nav-item/nav-item';
+
+const [navbarItems, setNavbarItems] = useState({
+    'items': [
+        {
+            'id': 1,
+            'text': 'Actividades',
+            'route': 'https://github.com/'
+        },
+        {
+            'id': 2,
+            'text': 'Actividades',
+            'route': 'https://github.com/'
+        },
+        {
+            'id': 3,
+            'text': 'Actividades',
+            'route': 'https://github.com/'
+        },
+    ]
+});
 
 const Header = () => {
     return (
