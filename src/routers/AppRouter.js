038e160e8af0d0ff042aa3e9373a33eb53ca8d97
gabrielgagz <g.a.gomez@gmail.com>
2021-04-27 22:58:48 -0300

    Add react router dom and create router to handle app routes
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { Counter } from '../features/counter/Counter';
+import App from '../App';
+
+export const AppRouter = () => {
+    return (
+
+        <Router>
+            <Switch>
+                <Route exact path='/' component={ App } />
+                { 
+                    /* TODO: use a real component, when the project grows up 
+                    Create private and public routes */ 
+                }
+                <Route exact path='/counter' component={ Counter } /> 
+                <Redirect to='/' />
+            </Switch>
+        </Router>
+
+    )
+}
