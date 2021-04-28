782f79e9722048f58824a91c1a1dce8dae463f20
BrunoCereda9979 <bruno9979@gmail.com>
2021-04-28 13:29:55 -0300

    Created the folder to hold the header component.

---
+++ src/components/header/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Styles
+import './header.styles.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="#"/>
+            </div>
+            <div className="nav-container">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header;
