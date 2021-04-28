2adeedfff80d584ebffa82a280e779caec2215fb
BrunoCereda9979 <bruno9979@gmail.com>
2021-04-28 15:38:37 -0300

    Mapped the navbar items using the mock state.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 
 // Styles
 import './header.styles.css';
@@ -6,36 +6,39 @@ import './header.styles.css';
 // Components
 import NavItem from '../nav-item/nav-item';
 
-const [navbarItems, setNavbarItems] = useState({
-    'items': [
-        {
-            'id': 1,
-            'text': 'Actividades',
-            'route': 'https://github.com/'
-        },
-        {
-            'id': 2,
-            'text': 'Actividades',
-            'route': 'https://github.com/'
-        },
-        {
-            'id': 3,
-            'text': 'Actividades',
-            'route': 'https://github.com/'
-        },
-    ]
-});
-
 const Header = () => {
+    const [navbarItems, setNavbarItems] = useState(
+        [
+            {
+                'id': 1,
+                'text': 'Actividades',
+                'route': 'https://github.com/'
+            },
+            {
+                'id': 2,
+                'text': 'Noticias',
+                'route': 'https://github.com/'
+            },
+            {
+                'id': 3,
+                'text': 'Contactanos',
+                'route': 'https://github.com/'
+            },
+        ]
+    );
+
     return (
         <div className="header">
             <div className="logo-container">
                 <img className="logo" src="#"/>
             </div>
             <div className="nav-container">
-                <NavItem itemText="Test" itemRoute="https://github.com/"/>
-                <NavItem itemText="Test" itemRoute="https://github.com/"/>
-                <NavItem itemText="Test" itemRoute="https://github.com/"/>
+                {
+
+                    navbarItems.map(item => {
+                        return <NavItem key={item.id} itemText={item.text} itemRoute={item.route}/>
+                    })
+                }
             </div>
         </div>
     )
