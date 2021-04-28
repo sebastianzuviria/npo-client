8f57841e9a67e61d951c998269d7062fb565bdf3
BrunoCereda9979 <bruno9979@gmail.com>
2021-04-28 13:35:19 -0300

    Imported the nav item component into the header component.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,6 +3,9 @@ import React from 'react';
 // Styles
 import './header.styles.css';
 
+// Components
+import NavItem from '../nav-item/nav-item';
+
 const Header = () => {
     return (
         <div className="header">
@@ -10,7 +13,7 @@ const Header = () => {
                 <img className="logo" src="#"/>
             </div>
             <div className="nav-container">
-
+                
             </div>
         </div>
     )
