c00f270665408fa810c42e40e04323611759953d
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-04-28 18:08:00 -0300

    contact view

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import ContactForm from '../components/Contact/ContactForm';
+import ContactInfo from '../components/Contact/ContactInfo';
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="mt-5 container">
+      <div className="mt-5 row d-flex align-items-start justify-content-center">
+        <div className="col-12 col-lg-6">
+          <ContactInfo />
+        </div>
+        <div className="col-12 col-lg-6">
+          <ContactForm />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
