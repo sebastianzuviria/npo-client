44123d682369b276d9986ef9f31190aed7aa9164
BrunoCereda9979 <bruno9979@gmail.com>
2021-04-28 13:33:31 -0300

    Created the basic nav item component.

---
+++ src/components/nav-item/nav-item.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Styles
+import './nav-item.styles.css';
+
+const NavItem = () => {
+    return (
+        <div>
+            <h1>Item</h1>
+        </div>
+    )
+}
+
+export default NavItem;
