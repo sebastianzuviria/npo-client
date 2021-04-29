6c732ff5c79b5c87a6bbe58b713f4f02c5d504c9
szuviria <szuviria@gmail.com>
2021-04-29 14:11:04 -0300

    Configured behavior to edit and create a New

--- src/components/NewsForm/NewsFrom.js
+++ src/components/NewsForm/NewsFrom.js
@@ -1,72 +1,95 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-const NewsForm = () => {
-    const [title, setTitle] = useState('');
-    const [image, setImage] = useState('');
-    const [content, setContent] = useState('');
-    const [category, setCategory] = useState('');
+const NewsForm = ({ id, title, image, category, content }) => {
+    const [newTitle, setNewTitle] = useState('');
+    const [newImage, setNewImage] = useState('');
+    const [newContent, setNewContent] = useState('');
+    const [newCategory, setNewCategory] = useState('');
+    const [isEdit, setIsEdit] = useState(false);
+    const [titleOfForm, setTitleOfForm] = useState('Create a New');  
 
-    console.log(content)
+    useEffect(() => {
+        const verifyContent = id && title && image && category && content;
+        const isContentToEdit = (verifyContent) ? true : false;
+        if (isContentToEdit) {
+            setIsEdit(true); 
+            setNewTitle(title);
+            setNewImage(image);
+            setNewCategory(category);
+            setNewContent(content);
+            setTitleOfForm('Edit a New');
+        }
+    },[id, title, image, category, content]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
         const newsObject = {
-            title,
-            image,
-            category,
-            content
-        }
+            newTitle,
+            newImage,
+            newCategory,
+            newContent
+        };
 
-        console.log(newsObject)
+        try {
+            if (isEdit) {
+                //const editedNew = await newsService.editNew(id, newsObject);
+                console.log(newsObject, 'edited New');
+            } else {
+                //const createdNew = await newsService.createNew(newObject);
+                console.log(newsObject, 'created New');
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
         <div>
-            <h2>News Form</h2>
+            <h2>{titleOfForm}</h2>
             <form style={{ display: 'flex', flexDirection: 'column'}} onSubmit={handleSubmit}>
                 <label>
                     title
                     <input 
                         type='text'
-                        value={title}
-                        onChange={({target}) => { setTitle(target.value) }}
+                        value={newTitle}
+                        onChange={({target}) => { setNewTitle(target.value) }}
                     />
                 </label>
                 <label>
                     image
                     <input 
                         type='text'
-                        value={image}
-                        onChange={({target}) => { setImage(target.value) }}
+                        value={newImage}
+                        onChange={({target}) => { setNewImage(target.value) }}
                     />
                 </label>
                 <label>
                     category
                     <input 
                         type='text'
-                        value={category}
-                        onChange={({target}) => { setCategory(target.value) }}
+                        value={newCategory}
+                        onChange={({target}) => { setNewCategory(target.value) }}
                     />
                 </label>
                 <label>
                     content
                     <CKEditor
                     editor={ ClassicEditor }
-                    data={content}
+                    data={newContent}
                     onChange={ ( event, editor ) => {
                         const data = editor.getData();
-                        setContent(data)
+                        setNewContent(data)
                     } }
                 />
                 </label>
                 <button type='submit'>Submit</button>
             </form>
         </div>
-    )
+    );
 
-}
+};
 
-export default NewsForm
+export default NewsForm;
