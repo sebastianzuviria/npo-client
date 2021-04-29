0f4c273f4cef3fa6f97b4d8c2e9665a320034bda
Emiliano Rivarola <emi_rivarola@hotmail.com>
2021-04-29 18:34:15 -0300

    I moved the .store file to the root src folder, modify the import of the store in the index and create the slices folder and inside I put the userSlice file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import store from './app/store';
+import store from './store';
 import AppRouter from './routers/AppRouter';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
