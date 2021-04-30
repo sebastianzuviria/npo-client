3d38c3e6e94ab53e1cfb00cf11cf908d1ed95ced
gabrielgagz <g.a.gomez@gmail.com>
2021-04-30 17:17:51 -0300

    Change to more descriptive names
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- src/services/apiGetService.js
+++ src/services/apiGetService.js
@@ -2,11 +2,11 @@ import apiService from './apiService';
 
 // Use 'type' for api sub-route) 
 // id isn't mandatory. Call Example: getService( 'users' )
-const getService = async ( type, id = '' ) => {
+const apiGetService = async ( type, id = '' ) => {
 
     const response = await apiService.get(`/${ type }/${ id }`);
     return await response.data;
 
 }
 
-export default getService;
+export default apiGetService;
--- src/services/apiPostService.js
+++ src/services/apiPostService.js
@@ -2,11 +2,11 @@ import apiService from './apiService';
 
 // Use 'type' for api sub-route and payload for object data
 // Call Example: postService( 'users', { ... } )
-const postService = async ( type, payload ) => {
+const apiPostService = async ( type, payload ) => {
 
     const response = await apiService.post(`/${ type }`, payload);
     return await response.data;
 
 }
 
-export default postService;
+export default apiPostService;
