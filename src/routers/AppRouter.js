4a5902e839cd6865cb05bc133d24bae2bb6ac893
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-03 16:30:06 -0300

    contact view  added

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 import { Counter } from '../features/counter/Counter';
-import UserList from '../views/UserList'
+import UserList from '../views/UserList';
+import Contact from '../views/Contact';
 import App from '../App';
 
 const AppRouter = () => {
-    return (
-
-        <Router>
-            <Switch>
-                <Route exact path='/' component={ App } />
-                <Route exact path='/backoffice/users' component={ UserList } />
-                { 
-                    /* TODO: use a real component, when the project grows up 
-                    Create private and public routes */ 
-                }
-                <Route exact path='/counter' component={ Counter } /> 
-                <Redirect to='/' />
-            </Switch>
-        </Router>
-
-    )
-}
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/backoffice/users" component={UserList} />
+        <Route exact path="/contacts" component={Contact} />
+        {/* TODO: use a real component, when the project grows up 
+                    Create private and public routes */}
+        <Route exact path="/counter" component={Counter} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default AppRouter;
