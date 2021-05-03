2671029c4f4afd5da65ef35f0d4025589d7edd8f
Bzch <francobaisch00@gmail.com>
2021-05-03 14:59:07 -0300

    created component for message

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,9 @@
 import React from "react";
+import WelcomeText from "../WelcomeText/WelcomeText";
 
 /*GET a /news. 
 Devolverá el listado de entries cuyo campo type sea "news". 
 Devolverá solamente los campos name, image y createdAt*/
-
-//welcome title
-let welcome = [
-  {
-    id: 1,
-    title: "Bienvenido a nuestra página",
-    text:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mattis libero non libero luctus feugiat. Aliquam erat volutpat. Integer et dapibus nibh, at aliquam urna. Fusce faucibus lacus sit amet tellus bibendum elementum. Phasellus elit nunc, ornare nec ultrices sit amet, imperdiet at eros. Praesent sem nisl, euismod vel urna ut, mollis gravida est. Etiam ut velit at leo venenatis. ",
-  },
-];
 
 let news = [
   {
@@ -57,15 +48,7 @@ let filteredNews = news.filter((lastNews) => lastNews.id < 5);
 function Home() {
   return (
     <div>
-      <div>
-        {welcome.map((content) => (
-          <div className="welcome-container" key={content.id}>
-            <div>{content.title}</div>
-            <br />
-            <div>{content.text}</div>
-          </div>
-        ))}
-      </div>
+      <WelcomeText />
       <br />
       <div key={filteredNews.id}>
         <div>
