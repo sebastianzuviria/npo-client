c9b7a36d740ce4ffda287a6613e83caf539c3d04
gabrielgagz <g.a.gomez@gmail.com>
2021-05-04 19:51:01 -0300

    Add fadeIn animation on component load
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import AppRouter from './routes/AppRouter';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import './App.css';
-import AppRouter from './routes/AppRouter'
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
 
 function App() {
   return (
     <div>
       <div>Welcome to Ong</div>
       <Header />
-      <AppRouter />
+      { /* This should be the main container.  */ }
+      <div className='animate__animated animate__fadeIn animate__delay-1s'>
+        <AppRouter />
+      </div>
       <Footer />
     </div>
   );
