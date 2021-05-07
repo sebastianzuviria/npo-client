ba94a76efcd9c37ced7b037c81e29b8b5b8bb75c
brunoo_cereda <bruno999@gmail.com>
2021-05-07 14:14:19 -0300

    Modified the components list to show a title and added an edit and delete buttons to make CRUD operations.

--- src/components/CategoriesList/CategoriesList.js
+++ src/components/CategoriesList/CategoriesList.js
@@ -4,13 +4,17 @@ import './CategoriesList.css';
 const CategoriesList = ({name}) => {
     return (
         <div className="categories-list">
+            <h1 className="list-title">Categories List</h1>
+            
+            {/* Puede ser transformado en un componente independiente */}
             <div className="category-container">
                 <div className="category-info">
-                    <p className="category-info__tag">Category Name:</p>
-                    <h2 className="category-info__name">{ name }</h2>
+                    <p className="category-info__tag">Category Name: </p>
+                    <p className="category-info__name">{ name }</p>
                 </div>
                 <div className="category-actions">
-                
+                    <button>Edit</button>
+                    <button>Delete</button>
                 </div>
             </div>
         </div>
