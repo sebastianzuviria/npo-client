b84acc97ab33fcc7695f7326e68e906e6f6a8049
brunoo_cereda <bruno999@gmail.com>
2021-05-07 12:49:15 -0300

    Created the boilerplate for the Categories List component.

---
+++ src/components/CategoriesList/CategoriesList.js
@@ -0,0 +1,12 @@
+import { React } from 'react';
+import './CategoriesList';
+
+const CategoriesList = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+module.exports = CategoriesList;
