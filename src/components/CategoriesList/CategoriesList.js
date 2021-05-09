e5eb496308bce303348dbbb85e0bc227a8653690
brunoo_cereda <bruno999@gmail.com>
2021-05-09 15:10:44 -0300

    Imported the Category component into CategoriesList and moved the css.

--- src/components/CategoriesList/CategoriesList.js
+++ src/components/CategoriesList/CategoriesList.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import  React from 'react';
+import { useEffect, useState } from 'react';
 import './CategoriesList.css';
+import Axios from 'axios';
+import Category from './Category/Category';
 
-const CategoriesList = ({name}) => {
+const CategoriesList = () => {
+    const [categories, setCategories] = useState([]);
+
+    const mockCategories = [
+        {
+            id: 1,
+            name: "Test Category 1",
+            description: "This is test category number 1"
+        },
+        {
+            id: 2,
+            name: "Test Category 2",
+            description: "This is test category number 2"
+        },
+        {
+            id: 3,
+            name: "Test Category 1",
+            description: "This is test category number 3"
+        },
+    ]
+
+    useEffect(() => {
+        Axios.get('http://localhost:3000/backoffice/contacts')
+        .then((res) => {
+            setCategories = res.data;
+        })
+    }, []);
+
     return (
         <div className="categories-list">
             <h1 className="list-title">Categories List</h1>
             
-            {/* Puede ser transformado en un componente independiente */}
-            <div className="category-container">
-                <div className="category-info">
-                    <p className="category-info__tag">Category Name: </p>
-                    <p className="category-info__name">{ name }</p>
-                </div>
-                <div className="category-actions">
-                    <button>Edit</button>
-                    <button>Delete</button>
-                </div>
-            </div>
+            {
+                mockCategories.forEach(category => {
+                    return (
+                        <Category key={category.id} name={ category.name } />
+                    )
+                })
+            }
         </div>
     )
 }
--- src/components/CategoriesList/Category/Category.js
+++ src/components/CategoriesList/Category/Category.js
@@ -3,7 +3,16 @@ import './Category.css';
 
 const Category = ({ name }) => {
     return (
-        <div></div>
+        <div className="category-container">
+            <div className="category-info">
+                <p className="category-info__tag">Category Name: </p>
+                <p className="category-info__name">{ name }</p>
+            </div>
+            <div className="category-actions">
+                <button>Edit</button>
+                <button>Delete</button>
+            </div>
+        </div>
     )
 }
 
