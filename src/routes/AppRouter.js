bd3a10619a50ff4ff6659992419b3bca1c508bf7
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-13 18:40:14 -0300

    Added backoffice routes to main router

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -5,50 +5,49 @@ import PrivateRoute from './components/PrivateRoute';
 import Contact from '../views/Contact';
 import NewsForm from '../components/NewsForm/NewsForm';
 import Home from '../views/Home';
-import UserList from '../views/UserList';
 import SignupForm from '../components/SignupForm/SignupForm';
-import Profile from '../components/Profile/Profile';
 import Login from '../components/Login/Login';
 import PublicRoute from './components/PublicRoute';
 import News from '../views/News';
 import DetailedNew from '../views/DetailedNew';
 import UpdateformOrganization from '../components/UpdateOrganization/UpdateOrganization';
-import DetailActivity from '../views/DetailActivity'
-import FormActivities from '../components/FormActivities/Form'
-import ActivitiesList from '../views/ActivitiesList'
+import DetailActivity from '../views/DetailActivity';
+import FormActivities from '../components/FormActivities/Form';
+import BackOfficeRoutes from './BackOfficeRoutes';
 
 const AppRouter = () => {
-
   return (
     <Router>
       <Switch>
-        <PublicRoute exact path='/users' component={ UserList } />
-        <PublicRoute exact path='/contacts' component={ Contact } />
+        <PublicRoute exact path="/contacts" component={Contact} />
         <PublicRoute exact path="/novedades" component={News} />
+        <PublicRoute exact path="/novedades/:id" component={DetailedNew} />
         <PublicRoute exact path="/activities/:id" component={DetailActivity} />
-        <PublicRoute exact path='/' component={ Home } />        
+        <PublicRoute exact path="/" component={Home} />
 
-        { /*  Restricted routes for logged users */ }
-        <PublicRoute exact path='/login' component={ Login } restricted={ true } fallback={ '/profile' } />
-        <PublicRoute exact path='/signup' component={ SignupForm } restricted={ true } fallback={ '/profile' } />
+        {/*  Restricted routes for logged users */}
+        <PublicRoute
+          exact
+          path="/login"
+          component={Login}
+          restricted={true}
+          fallback={'/profile'}
+        />
+        <PublicRoute
+          exact
+          path="/signup"
+          component={SignupForm}
+          restricted={true}
+          fallback={'/profile'}
+        />
 
-        { /*  Private routes for logged users */ }
-        <PrivateRoute exact path='/profile' component={ Profile } />
+        {/*  TODO: implement admin routes */}
+        <BackOfficeRoutes />
 
-        { /*  TODO: implement admin routes */ }
-        <PrivateRoute exact path='/newsform' component={ NewsForm } />
-        <PrivateRoute exact path="/novedades/:id" component={ DetailedNew } />
-        <PrivateRoute exact path='/updateorganization' component={ UpdateformOrganization } />
-        <PublicRoute exact path="/FormActivities" component={FormActivities} />
-        <PublicRoute exact path="/backoffice/activities" component={ActivitiesList} />
-
-
-        <Redirect to='/' />
+        <Redirect to="/" />
       </Switch>
     </Router>
-
-    )
-}
-
+  );
+};
 
 export default AppRouter;
