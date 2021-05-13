aed5f2056963ff12ce2e19528df9b6dd7bcff39f
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-13 19:12:42 -0300

    Dinamic routes for users and admins

--- src/components/BackOffice/Sidebar/Sidebar.js
+++ src/components/BackOffice/Sidebar/Sidebar.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import { isLogged } from '../../../slices/userSlice';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { isLogged, userLogged } from '../../../slices/userSlice';
 import './Sidebar.css';
 import SideLink from './SideLink';
-import { useDispatch } from 'react-redux';
+import store from '../../../store';
 
 const Sidebar = () => {
-  const dispatch = useDispatch;
-  console.log(dispatch(isLogged));
+  const [user, setUser] = useState();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(isLogged());
+    setUser(userLogged(store.getState()));
+  }, []);
+
   return (
     <div className="col-md-4 col-lg-3 col-xl-2 p-0 sidebar bg-light">
       <ul className="nav flex-column">
         <SideLink path="/backoffice" name="Perfil" />
-        <SideLink path="/backoffice/usuarios" name="Usuarios" />
-        <SideLink path="/backoffice/categorias" name="Categorias" />
-        <SideLink path="/backoffice/actividades" name="Actividades" />
-        <SideLink path="/backoffice/novedades" name="Novedades" />
-        <SideLink path="/backoffice/organizacion" name="Organizacion" />
+        {user && user.roleId === 1 && (
+          <>
+            <SideLink path="/backoffice/usuarios" name="Usuarios" />
+            <SideLink path="/backoffice/categorias" name="Categorias" />
+            <SideLink path="/backoffice/actividades" name="Actividades" />
+            <SideLink path="/backoffice/novedades" name="Novedades" />
+            <SideLink path="/backoffice/organizacion" name="Organizacion" />
+          </>
+        )}
       </ul>
     </div>
   );
