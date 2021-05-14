8d442dfb79b5644cfb1a4adcb6d880d8a3392ccb
Nataly Mora Mancera <tatamora21@gmail.com>
2021-05-14 09:59:26 -0500

    Updated component showing categories

--- src/components/CategoriesList/CategoriesList.js
+++ src/components/CategoriesList/CategoriesList.js
@@ -1,50 +1,36 @@
-import  React from 'react';
-import { useEffect, useState } from 'react';
-import './CategoriesList.css';
-import Axios from 'axios';
-import Category from './Category/Category';
+import React from "react";
+import { useEffect, useState } from "react";
+import "./CategoriesList.css";
+import apiGetService from "../../services/apiGetService";
+import Category from "./Category/Category";
 
 const CategoriesList = () => {
     const [categories, setCategories] = useState([]);
 
-    const mockCategories = [
-        {
-            id: 1,
-            name: "Test Category 1",
-            description: "This is test category number 1"
-        },
-        {
-            id: 2,
-            name: "Test Category 2",
-            description: "This is test category number 2"
-        },
-        {
-            id: 3,
-            name: "Test Category 1",
-            description: "This is test category number 3"
-        },
-    ]
+    const allcategories = async () => {
+        const info = await apiGetService('categories');
+        setCategories(info);
+    };
 
     useEffect(() => {
-        Axios.get('http://localhost:3000/backoffice/contacts')
-        .then((res) => {
-            setCategories = res.data;
-        })
+        allcategories();
     }, []);
 
     return (
         <div className="categories-list">
             <h1 className="list-title">Categories List</h1>
-            
-            {
-                mockCategories.forEach(category => {
-                    return (
-                        <Category key={category.id} name={ category.name } />
-                    )
-                })
-            }
+
+            {categories.length > 0 ? (
+                categories.map((item) => (
+                <div>
+                    <Category key={item.id} name={item.name} id={item.id} />
+                </div>
+                ))
+            ) : (
+                <p>There is no registered categories</p>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default CategoriesList;
