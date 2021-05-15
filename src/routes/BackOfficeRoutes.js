bc0a0dd238d41ac2d60a79fc613cdf283c9e5c0a
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-14 22:25:55 -0300

    Backoffice routes added

--- src/routes/BackOfficeRoutes.js
+++ src/routes/BackOfficeRoutes.js
@@ -6,33 +6,47 @@ import Activities from '../views/Activities';
 import Categories from '../views/Categories';
 import BackofficeNews from '../views/BackofficeNews';
 import Organization from '../views/Organization';
+import PublicRoute from './components/PublicRoute';
+import { shallowEqual, useSelector } from 'react-redux';
+import { userLogged } from '../slices/userSlice';
 
 const BackOfficeRoutes = () => {
+  //const user = useSelector(userLogged, shallowEqual);
   return (
-    <React.Fragment>
-      <PrivateRoute exact path="/backoffice" component={BackOfficeLayout} />
-      <PrivateRoute exact path="/backoffice/usuarios" component={UserList} />
+    <>
+      <PublicRoute exact path="/backoffice" component={BackOfficeLayout} />
+      <PrivateRoute
+        exact
+        path="/backoffice/usuarios"
+        component={UserList}
+        role="Admin"
+      />
+
       <PrivateRoute
         exact
         path="/backoffice/actividades"
         component={Activities}
+        role="Admin"
       />
       <PrivateRoute
         exact
         path="/backoffice/novedades"
         component={BackofficeNews}
+        role="Admin"
       />
       <PrivateRoute
         exact
         path="/backoffice/organizacion"
         component={Organization}
+        role="Admin"
       />
       <PrivateRoute
         exact
         path="/backoffice/categorias"
         component={Categories}
+        role="Admin"
       />
-    </React.Fragment>
+    </>
   );
 };
 
