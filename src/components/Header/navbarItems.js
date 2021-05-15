669f3b936b8dfd43bacb123e288105db50b5a896
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-15 17:19:42 -0300

    added navbarItems for navbar

---
+++ src/components/Header/navbarItems.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    text: 'Home',
+    route: '/'
+  },
+  { text: 'News', route: '/news' },
+  {
+    text: 'Contact',
+    route: '/contact'
+  },
+  {
+    text: 'Backoffice',
+    route: '/backoffice'
+  },
+  { text: 'Login', route: '/login' },
+  {
+    text: 'Signup',
+    route: '/signup'
+  }
+];
