d5df953fa0687f340bd36fa1e63d957ae8c1978d
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-14 22:22:55 -0300

    added backoffice route to AppRouter

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,29 +1,24 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Redirect, Switch } from 'react-router-dom';
-import PrivateRoute from './components/PrivateRoute';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Contact from '../views/Contact';
-import NewsForm from '../components/NewsForm/NewsForm';
 import Home from '../views/Home';
 import SignupForm from '../components/SignupForm/SignupForm';
 import Login from '../components/Login/Login';
 import PublicRoute from './components/PublicRoute';
 import News from '../views/News';
 import DetailedNew from '../views/DetailedNew';
-import UpdateformOrganization from '../components/UpdateOrganization/UpdateOrganization';
 import DetailActivity from '../views/DetailActivity';
-import FormActivities from '../components/FormActivities/Form';
 import BackOfficeRoutes from './BackOfficeRoutes';
 
 const AppRouter = () => {
   return (
     <Router>
       <Switch>
-        <PublicRoute exact path="/contacts" component={Contact} />
-        <PublicRoute exact path="/novedades" component={News} />
-        <PublicRoute exact path="/novedades/:id" component={DetailedNew} />
+        <Route exact path="/contacts" component={Contact} />
+        <Route exact path="/novedades" component={News} />
+        <Route exact path="/novedades/:id" component={DetailedNew} />
         <PublicRoute exact path="/activities/:id" component={DetailActivity} />
-        <PublicRoute exact path="/" component={Home} />
 
         {/*  Restricted routes for logged users */}
         <PublicRoute
@@ -31,20 +26,20 @@ const AppRouter = () => {
           path="/login"
           component={Login}
           restricted={true}
-          fallback={'/profile'}
+          fallback={'/'}
         />
         <PublicRoute
           exact
           path="/signup"
           component={SignupForm}
           restricted={true}
-          fallback={'/profile'}
+          fallback={'/'}
         />
 
         {/*  TODO: implement admin routes */}
         <BackOfficeRoutes />
 
-        <Redirect to="/" />
+        <Route exact path="/" component={Home} />
       </Switch>
     </Router>
   );
