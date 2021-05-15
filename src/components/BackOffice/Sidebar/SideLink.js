507bd7fb62525608acaa8ad679a5d6f8e249deb2
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-15 16:05:16 -0300

    SideLink now return a NavLink

--- src/components/BackOffice/Sidebar/SideLink.js
+++ src/components/BackOffice/Sidebar/SideLink.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const SideLink = ({ path, name }) => {
   return (
-    <Link to={path} className="nav-link bg-info p-2 text-white">
+    <NavLink to={path} className="nav-link bg-info p-2 text-white">
       {name}
-    </Link>
+    </NavLink>
   );
 };
 
