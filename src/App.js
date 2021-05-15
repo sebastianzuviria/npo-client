707143baee9d28a6625e5dcb9f469b0c03f12584
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-15 17:14:09 -0300

    App.js now only returns AppRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import AppRouter from './routes/AppRouter';
-import Footer from './components/Footerdinamic/Footer';
-import Header from './components/Header/Header';
 import './App.css';
 import { useDispatch } from 'react-redux';
 import { isLogged } from './slices/userSlice';
+//import Footer from './components/Footerdinamic/Footer';
+//import Header from './components/Header/Header';
 
 function App() {
   const dispatch = useDispatch();
@@ -12,15 +12,20 @@ function App() {
     dispatch(isLogged());
   });
   return (
-    <div>
-      <div>Welcome to Ong</div>
-      <Header />
-      <div className="animate__animated animate__fadeIn">
-        <AppRouter />
-      </div>
-      <Footer />
+    <div className="animate__animated animate__fadeIn">
+      <AppRouter />
     </div>
   );
+  //return (
+  //<div>
+  //<div>Welcome to Ong</div>
+  //<Header />
+  //<div className="animate__animated animate__fadeIn">
+  //<AppRouter />
+  //</div>
+  //<Footer />
+  //</div>
+  //);
 }
 
 export default App;
