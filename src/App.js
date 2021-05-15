044795e460c2e1ea265384cbef4e9cba3b345abd
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-14 22:21:30 -0300

    added redux to app.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import AppRouter from './routes/AppRouter';
 import Footer from './components/Footerdinamic/Footer';
 import Header from './components/Header/Header';
 import './App.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { isLogged } from './slices/userSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(isLogged());
+  }, []);
+
   return (
     <div>
       <div>Welcome to Ong</div>
       <Header />
-      <div className='animate__animated animate__fadeIn'>
+      <div className="animate__animated animate__fadeIn">
         <AppRouter />
       </div>
       <Footer />
