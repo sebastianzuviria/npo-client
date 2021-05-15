f416280487db43229f3ba2a77db9ede313e4adcb
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-14 22:25:39 -0300

    Modified PublicRoute to check if the user is registered or if the URL is restricted

--- src/routes/components/PublicRoute.js
+++ src/routes/components/PublicRoute.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-import userLogged from '../../helpers/userLogged';
+import { userLogged } from '../../slices/userSlice';
 
-const PublicRoute = ({ component: Component, restricted, fallback = '/', ...rest }  ) => {
-
-    const userIsLogged = userLogged();
-
-    return (
-
-    <Route {...rest} render= { props => (
-
-        userIsLogged && restricted ?  <Redirect to={ fallback } /> : <Component { ...props } />
-
-    ) } />
-
-    )
-}
+const PublicRoute = ({
+  component: Component,
+  fallback = '/',
+  restricted = false,
+  ...rest
+}) => {
+  const user = useSelector(userLogged);
+  // if (user) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        (user && !restricted) || !user ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: fallback, state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+  //} else {
+  // return <div>kasdjf</div>;
+  // }
+};
 
 export default PublicRoute;
