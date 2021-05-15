16dec5128182ff797d315aa998b5d02d12fefa8d
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-15 17:20:31 -0300

    Modified routes of BackOffficeRoutes

--- src/routes/BackOfficeRoutes.js
+++ src/routes/BackOfficeRoutes.js
@@ -1,48 +1,44 @@
 import React from 'react';
 import PrivateRoute from './components/PrivateRoute';
-import BackOfficeLayout from '../components/BackOffice/BackOfficeLayout';
-import UserList from '../views/UserList';
-import Activities from '../views/Activities';
-import Categories from '../views/Categories';
-import BackofficeNews from '../views/BackofficeNews';
-import Organization from '../views/Organization';
-import PublicRoute from './components/PublicRoute';
-import { shallowEqual, useSelector } from 'react-redux';
-import { userLogged } from '../slices/userSlice';
+import UserList from '../views/Backoffice/UserList';
+import Activities from '../views/Backoffice/Activities';
+import Categories from '../views/Backoffice/Categories';
+import BackofficeNews from '../views/Backoffice/News';
+import Organization from '../views/Backoffice/Organization';
+import BackOffice from '../views/Backoffice/BackOffice';
 
 const BackOfficeRoutes = () => {
-  //const user = useSelector(userLogged, shallowEqual);
   return (
     <>
-      <PublicRoute exact path="/backoffice" component={BackOfficeLayout} />
+      <PrivateRoute exact path="/backoffice" component={BackOffice} />
       <PrivateRoute
         exact
-        path="/backoffice/usuarios"
+        path="/backoffice/users"
         component={UserList}
         role="Admin"
       />
 
       <PrivateRoute
         exact
-        path="/backoffice/actividades"
+        path="/backoffice/activities"
         component={Activities}
         role="Admin"
       />
       <PrivateRoute
         exact
-        path="/backoffice/novedades"
+        path="/backoffice/news"
         component={BackofficeNews}
         role="Admin"
       />
       <PrivateRoute
         exact
-        path="/backoffice/organizacion"
+        path="/backoffice/organization"
         component={Organization}
         role="Admin"
       />
       <PrivateRoute
         exact
-        path="/backoffice/categorias"
+        path="/backoffice/categories"
         component={Categories}
         role="Admin"
       />
