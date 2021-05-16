aa8189b418b14fdacd2c7ef78ffd9bb230c303cb
jonathan <jonathangomezit@gmail.com>
2021-05-16 17:31:42 -0300

    added: apiDeleteService

---
+++ src/services/apiDeleteService.js
@@ -0,0 +1,11 @@
+import apiService from './apiService';
+
+
+const apiDeleteService = async ( type, id ) => {
+
+    const response = await apiService.delete(`/${ type }/ ${id}`);
+    return await response.data;
+
+}
+
+export default apiDeleteService;
