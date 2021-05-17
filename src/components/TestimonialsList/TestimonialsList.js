69b027d55afa4ce14c181bc41b0660d6a174fa52
jonathan <jonathangomezit@gmail.com>
2021-05-16 21:09:26 -0300

    added modify and delete with alerts

--- src/components/TestimonialsList/TestimonialsList.js
+++ src/components/TestimonialsList/TestimonialsList.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import {confirmAlert, successAlert} from '../Alert/Alert';
 import apiGetService from '../../services/apiGetService';
+import apiDeleteService from '../../services/apiDeleteService';
 import TestimonialItem from './TestimonialItem';
 
 const TestimonialsList = () => {
 
     const [testimonials, setTestimonials] = useState([]);
-    
+
     const getTestimonials = async () => {
         try{
             const res = await apiGetService('testimonials');
@@ -14,17 +16,50 @@ const TestimonialsList = () => {
             console.log(err)
         }
     }
+    const deleteTestimonial = async (id) => {
+        
+        try{
+            const confirm = await confirmAlert();
+            
+            if(confirm.isConfirmed) {
+
+                const res = await apiDeleteService('testimonials', id)
+
+                setTestimonials(testimonials.filter( value => value.id !== id))
+
+                return await successAlert();
+            }
+            
+        }catch(err){
+            console.log(err)
+        }
+    }
+    
+    // const handleCancel = () => {
+    //     console.log('cancel')
+    //     getTestimonials();
+
+    // }
 
     useEffect(() => {
 
         getTestimonials();
 
     },[])
+
     
     return(
         <div className="TestimonialsList__content">
+
             {
-                testimonials.map(value => <TestimonialItem key={value.id} {...value} />)
+                testimonials.map(value => <TestimonialItem key={value.id} {...value} 
+
+                    deleteTestimonial={ () => deleteTestimonial(value.id) }
+                    
+                    
+                    />
+                    
+                )
             }
         </div>
     )
