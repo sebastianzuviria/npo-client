c99426e2f050e21498e000e3d0c018556b72a3e4
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-18 23:50:55 -0300

    Added styles and alerts to Login and SIgnup forms

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,87 +1,93 @@
 import React from 'react';
-import {Formik, Form, Field} from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { userLogin } from '../../slices/userSlice';
 import apiPostService from '../../services/apiPostService';
-import { useHistory } from 'react-router-dom';
-import { errorAlert } from '../Alert/Alert';
+import { Link, useHistory } from 'react-router-dom';
+import { errorAlert, successAlert } from '../Alert/Alert';
 import './Login.css';
 
 const Login = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    const history = useHistory();
+  const initialValues = {
+    email: '',
+    password: ''
+  };
 
-    const dispatch = useDispatch();
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email('Email no válido.').required('Campo requerido.'),
+    password: Yup.string()
+      .min(6, 'Debe tener un mínimo de 6 caracteres.')
+      .required('Campo requerido.')
+  });
 
-    const initialValues = {
-        email:'',
-        password:''
+  const handleSubmit = async (values) => {
+    try {
+      const authResponse = await apiPostService('auth/login', values);
+      dispatch(userLogin(authResponse));
+      await successAlert();
+      history.push('/');
+    } catch (err) {
+      await errorAlert();
     }
+  };
 
-    const validationSchema = Yup.object().shape({
-        email: Yup.string()
-        .email('Please enter valid email')
-        .required('Required'),
-        password: Yup.string()
-        .min(6, 'The password must be at least 6 characters')
-        .required('Required')
-    });
-
-    const handleSubmit = async ( values ) => {
-
-            try {
-
-                const authResponse = await apiPostService('auth/login', values);
-                dispatch( userLogin( authResponse ) );
-                history.push('/');  
-                
-            } catch (err) {
-    
-                await errorAlert();
-            }
-
-    }
-
-    return (
-        <div className="d-flex justify-content-center align-items-center divF-form">
-            <Formik 
-                initialValues={initialValues} 
-                onSubmit={ handleSubmit } 
-                validationSchema={validationSchema}
-            >
-                {({errors, touched})=>(
-                    <Form>
-                        <div className="form-login">
-                            <h2 className="text-center mb-4">Sign In</h2>
-                            <Field 
-                                className="form-control" 
-                                type="email" 
-                                placeholder="Email *" 
-                                name="email"
-                            />
-                            {errors.email && touched.email ? <div className="error-message">{errors.email}</div> : null}
-                            <br/>
-                            <Field 
-                                className="form-control" 
-                                type="password" 
-                                placeholder="Password *" 
-                                name="password"
-                            />
-                            {errors.password && touched.password ? <div className="error-message">{errors.password}</div> : null}
-                            <br/>
-                            <div className="d-flex justify-content-center">
-                                <button className="btn btn-primary mb-3" type='submit'>Sign In</button>
-                            </div>
-                            <div className="d-flex justify-content-center">
-                                <button className="btn btn-primary" type='submit'>Register</button>
-                            </div>
-                        </div>
-                    </Form>
-                )}
-            </Formik>
-        </div>
-    )
-}
+  return (
+    <div className="container d-flex wrapper">
+      <div className="row align-items-center justify-content-center vw-100 mx-1">
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validationSchema={validationSchema}
+        >
+          <Form className="card shadow col-md-8 col-lg-6">
+            <div className="card-body">
+              <h2 className="card-title py-3">Inicia sesión</h2>
+              <div className="form-floating mt-4">
+                <Field
+                  className="form-control"
+                  type="email"
+                  placeholder="Email *"
+                  name="email"
+                  id="email"
+                />
+                <label htmlFor="email">Email</label>
+                <ErrorMessage
+                  name="email"
+                  className="invalid-feedback ml-2 d-block"
+                  component="div"
+                />
+              </div>
+              <div className="form-floating mt-4">
+                <Field
+                  className="form-control"
+                  type="password"
+                  placeholder="Password *"
+                  name="password"
+                  id="password"
+                />
+                <label htmlFor="password">Contraseña</label>
+                <ErrorMessage
+                  name="password"
+                  className="invalid-feedback ml-2 d-block"
+                  component="div"
+                />
+              </div>
+              <button className="w-100 btn-lg btn-primary my-3" type="submit">
+                Inicia sesión
+              </button>
+              <div className="form-text my-2">
+                No tenes usuario? <Link to="/signup">Registrate acá</Link>
+              </div>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
