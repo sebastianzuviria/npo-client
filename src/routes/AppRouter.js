810c64c5ceecd4210e7f2d2d0f3f4009931a9ff5
Nataly Mora Mancera <tatamora21@gmail.com>
2021-05-19 22:46:09 -0500

    Ajusted routes publics and private

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -6,12 +6,6 @@ import RestrictedRoute from './components/RestrictedRoute';
 import News from '../views/News';
 import DetailedNew from '../views/DetailedNew';
 import DetailActivity from '../views/DetailActivity';
-import FormActivities from '../components/FormActivities/FormContent'
-import ActivitiesList from '../views/ActivitiesList'
-import CategoriesList from '../components/CategoriesList/CategoriesList'
-import HomeEditForm from '../components/HomeEditForm/HomeEditForm';
-import TestimonialsList from '../components/TestimonialsList/TestimonialsList';
-import TestimonialsForm from '../components/TestimonialsForm/TestimonialsForm';
 import BackOfficeRoutes from './BackOfficeRoutes';
 import Signup from '../views/Signup';
 import Login from '../views/Login';
@@ -31,35 +25,6 @@ const AppRouter = () => {
         {/*  Restricted routes for logged users */}
         <RestrictedRoute exact path="/login" component={Login} />
         <RestrictedRoute exact path="/signup" component={Signup} />
-
-        { /*  TODO: implement admin routes */ }
-        <PrivateRoute exact path='/newsform' component={ NewsForm } />
-        <PrivateRoute exact path="/novedades/:id" component={ DetailedNew } />
-        <PrivateRoute exact path='/updateorganization' component={ UpdateformOrganization } />
-        <PublicRoute exact path="/FormActivities" component={FormActivities} />
-        <PublicRoute exact path="/FormCategories" component={FormCategories} />
-        <PublicRoute exact path="/backoffice/activities" component={ActivitiesList} />
-        <PrivateRoute exact path='/categories' component={ CategoriesList } />
-        <PublicRoute exact path="/backoffice/editHome" component={ HomeEditForm } />
-
-        {/*  TODO: implement admin routes */}
-        <PrivateRoute
-          exact
-          path='/updateorganization'
-          component={UpdateformOrganization}
-        />
-        <PublicRoute
-          exact
-          path='/testimonialsform'
-          component={TestimonialsForm}
-        />
-        <PublicRoute exact path='/FormActivities' component={FormActivities} />
-        <PublicRoute
-          exact
-          path='/backoffice/activities'
-          component={ActivitiesList}
-        />
-        <PublicRoute exact path="/backoffice/testimonials" component={ TestimonialsList } />
 
         <BackOfficeRoutes />
       </Switch>
