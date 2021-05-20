201a2d6174ec2db888dc4c7617130d96e9d5f05b
gabrielgagz <g.a.gomez@gmail.com>
2021-05-20 20:32:18 -0300

    PUBLIC: migrate to Bootstrap 5

--- src/components/GetNews/GetNews.js
+++ src/components/GetNews/GetNews.js
@@ -2,43 +2,49 @@ import React, { useEffect, useState } from 'react';
 import apiGetService from '../../services/apiGetService';
 import './News.css';
 
+const GetNews = () => {
 
-function GetNews() {
+    const [news, setNews] = useState([]);
 
-  const [news, setNews] = useState([]);
+    const fetchNews = async () => {
+        const data = await apiGetService('news');
+        if (data) setNews(data);
+    };
 
-  const fetchNews = async () => {
-    const data = await apiGetService('news');
-    if (data) setNews(data);
-  };
+    useEffect(() => {
+        fetchNews();
+    }, []);
 
-  useEffect(() => {
-    fetchNews();
-  }, []);
-
-  return (
-
-  <div className='card-deck p-5'>
-    {
-      news.length > 0 && 
-        news.map( ({ id, image, title }) => (
-          
-      <div className='card border-0'>
-        <img className='card-img-top' src={image} alt={image}/>
-        <div className='card-body'>
-          <h5 className='card-title news__title'>{title}</h5>
-        <hr/>
-          <p className='card-text text-muted'>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    return (
+      <div className='container'>
+        <div className='row row-cols-1 row-cols-md-3 my-4'>
+            {news.length > 0 &&
+                news.map(({ id, image, title }) => (
+                    <div className='col'>
+                        <div className='card h-100 border-0'>
+                            <img
+                                className='card-img-top'
+                                src={image}
+                                alt={image}
+                            />
+                            <div className='card-body'>
+                                <h5 className='card-title news__title'>
+                                    {title}
+                                </h5>
+                                <hr />
+                                <p className='card-text text-muted'>
+                                    This is a wider card with supporting text
+                                    below as a natural lead-in to additional
+                                    content. This content is a little bit
+                                    longer.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
         </div>
-    </div>
-
-        ))
-      }
-  </div>
-
-  );
-
+        </div>
+    );
 }
-
 
 export default GetNews;
