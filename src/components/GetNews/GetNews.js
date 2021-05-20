6927f2f58d9d4ced411b498fde9dab0a3bb41d24
gabrielgagz <g.a.gomez@gmail.com>
2021-05-20 20:10:36 -0300

    NEWS: give news list a small refresh

--- src/components/GetNews/GetNews.js
+++ src/components/GetNews/GetNews.js
@@ -1,156 +1,40 @@
-import React from "react";
-
-//import axios from "axios";
-
-
-/*GET /news. 
-
-axios.get('/news', {
-
-    params: {
-
-      name: '',
-
-      image: '',
-
-      createdAt: ''
-
-    }
-
-  })
-
-  .then(function (response) {
-
-  })
-
-*/
-
-
-//mockup array
-
-let news = [
-
-  {
-
-    id: 1,
-
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-
-    image:
-
-      "https://bitbucket.org/alkemy-dev/t27-client/raw/0e3ef885e245070cba961e22b3553acc08c99dec/public/images/campaign-recent-02.jpg",
-
-    createdAt: "03/04/2021",
-
-  },
-
-  {
-
-    id: 2,
-
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-
-    image:
-
-      "https://bitbucket.org/alkemy-dev/t27-client/raw/0e3ef885e245070cba961e22b3553acc08c99dec/public/images/campaign-recent-02.jpg",
-
-    createdAt: "03/04/2021",
-
-  },
-
-  {
-
-    id: 3,
-
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-
-    image:
-
-      "https://bitbucket.org/alkemy-dev/t27-client/raw/0e3ef885e245070cba961e22b3553acc08c99dec/public/images/campaign-recent-02.jpg",
-
-    createdAt: "03/04/2021",
-
-  },
-
-  {
-
-    id: 4,
-
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-
-    image:
-
-      "https://bitbucket.org/alkemy-dev/t27-client/raw/0e3ef885e245070cba961e22b3553acc08c99dec/public/images/campaign-recent-02.jpg",
-
-    createdAt: "03/04/2021",
-
-  },
-
-  {
-
-    id: 5,
-
-    name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-
-    image:
-
-      "https://bitbucket.org/alkemy-dev/t27-client/raw/0e3ef885e245070cba961e22b3553acc08c99dec/public/images/campaign-recent-02.jpg",
-
-    createdAt: "03/04/2021",
-
-  },
-
-];
-
-
-//filtered array
-
-let filteredNews = news.filter((lastNews) => lastNews.id < 5);
+import React, { useEffect, useState } from 'react';
+import apiGetService from '../../services/apiGetService';
+import './News.css';
 
 
 function GetNews() {
 
+  const [news, setNews] = useState([]);
+
+  const fetchNews = async () => {
+    const data = await apiGetService('news');
+    if (data) setNews(data);
+  };
+
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
   return (
 
-    <div>
-
-      <div>NOVEDADES</div>
-
-      <br />
-
-      <div key={filteredNews.id}>
-
-        <div>
-
-          {filteredNews.map((filteredNews) => (
-
-            <div key={filteredNews.id}>
-
-              <div>{filteredNews.name}</div>
-
-              <br />
-
-              <div>
-
-                <img src={filteredNews.image} alt="newsImage"></img>
-
-              </div>
-
-              <br />
-
-              <div>{filteredNews.createdAt}</div>
-
-              <br />
-
-            </div>
-
-          ))}
-
+  <div className='card-deck p-5'>
+    {
+      news.length > 0 && 
+        news.map( ({ id, image, title }) => (
+          
+      <div className='card border-0'>
+        <img className='card-img-top' src={image} alt={image}/>
+        <div className='card-body'>
+          <h5 className='card-title news__title'>{title}</h5>
+        <hr/>
+          <p className='card-text text-muted'>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         </div>
-
-      </div>
-
     </div>
+
+        ))
+      }
+  </div>
 
   );
 
