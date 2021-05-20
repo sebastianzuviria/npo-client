b05ec934d4a3f222211b1c89b0be61145dd3abe9
gabrielgagz <g.a.gomez@gmail.com>
2021-05-20 01:45:33 -0300

    CSS: use Google Font Lato / HOME: improve welcome text

--- src/components/WelcomeText/WelcomeText.js
+++ src/components/WelcomeText/WelcomeText.js
@@ -1,22 +1,17 @@
 import React from "react";
 import './WelcomeText.css';
 
-//welcome title
-
-const welcome = {
-
-    title: "¡Bienvenido a nuestra página!",
-    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mattis libero non libero luctus feugiat. Aliquam erat volutpat. Integer et dapibus nibh, at aliquam urna. Fusce faucibus lacus sit amet tellus bibendum elementum. Phasellus elit nunc, ornare nec ultrices sit amet, imperdiet at eros. Praesent sem nisl, euismod vel urna ut, mollis gravida est. Etiam ut velit at leo venenatis. ",
-
-};
-
 const WelcomeText = () => {
 
   return (
 
     <div className="container text-center my-5">
-          <h1>{welcome.title}</h1>
-          <p className="text-muted mt-4">{welcome.text}</p>
+      <h1 className='welcomeText__title'>
+        ¡Bienvenid@ a nuestra página!
+      </h1>
+      <p className="lead mt-4">
+        Lorem ipsum dolor sit amet, consectetur <strong>adipiscing</strong> elit. Vestibulum mattis libero non libero luctus feugiat. Aliquam erat volutpat. Integer et dapibus nibh, at aliquam urna. Fusce faucibus lacus sit amet <strong>tellus</strong> bibendum elementum. Phasellus elit nunc, ornare nec <strong>ultrices</strong> sit amet, imperdiet at eros. Praesent sem nisl, euismod vel urna ut, mollis gravida est. <strong>Etiam</strong> ut velit at leo venenatis.
+      </p>
     </div>
   
   );
