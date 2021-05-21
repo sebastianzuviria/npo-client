3ca2122bdd6487937c956dfc005f6cedcacd4350
gabrielgagz <g.a.gomez@gmail.com>
2021-05-21 11:47:31 -0300

    GLOBAL: add 'Go to Top' button

---
+++ src/components/TopButton/TopButton.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './TopButton.css';
+
+export const TopButton = () => {
+
+    const [visible, setVisible] = useState(false)
+  
+    const toggleVisibility = () => {
+
+        const scrolled = document.documentElement.scrollTop;
+        (scrolled > 300) ? setVisible( true ) : setVisible( false ); 
+
+    }
+
+    const goToTop = () =>{
+
+        window.scrollTo({
+          top: 0, 
+          behavior: 'smooth'
+        });
+
+    }
+
+    window.addEventListener('scroll', toggleVisibility);
+
+    return (
+        <button
+            className={ `goToTopButton__btn px-3 py-2 rounded-circle ${visible ? 'goToTopButton__btn__show' : 'goToTopButton__btn__hidden' }` }
+            onClick={ goToTop }
+        >
+            <i class='fas fa-arrow-up'></i>
+        </button>  
+    )
+}
--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import store from './store';
 import App from './App';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { TopButton } from './components/TopButton/TopButton';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <TopButton />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
