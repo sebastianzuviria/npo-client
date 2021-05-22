8573ba2c263cd200b92945be982a9a52516c8c13
gabrielgagz <g.a.gomez@gmail.com>
2021-05-22 17:14:48 -0300

    HOME: make Slider and WelcomeText more responsive

--- src/components/WelcomeText/WelcomeText.js
+++ src/components/WelcomeText/WelcomeText.js
@@ -9,7 +9,7 @@ const WelcomeText = () => {
       <h1 className='welcomeText__title'>
         ¡Bienvenid@ a nuestra página!
       </h1>
-      <p className="lead mt-4">
+      <p className="lead mt-4 welcomeText__text">
         Lorem ipsum dolor sit amet, consectetur <strong>adipiscing</strong> elit. Vestibulum mattis libero non libero luctus feugiat. Aliquam erat volutpat. Integer et dapibus nibh, at aliquam urna. Fusce faucibus lacus sit amet <strong>tellus</strong> bibendum elementum. Phasellus elit nunc, ornare nec <strong>ultrices</strong> sit amet, imperdiet at eros. Praesent sem nisl, euismod vel urna ut, mollis gravida est. <strong>Etiam</strong> ut velit at leo venenatis.
       </p>
     </div>
