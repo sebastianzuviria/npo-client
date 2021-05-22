bfbf4488dbb36d09f006494c9073cc0f3b383dd7
gabrielgagz <g.a.gomez@gmail.com>
2021-05-22 14:44:42 -0300

    LAYOUT: implement lazy loading

--- src/Layouts/BasicLayout.js
+++ src/Layouts/BasicLayout.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Footer from '../components/Footerdinamic/Footer';
 import Header from '../components/Header/Header';
+import { LazyLoad } from 'react-observer-api';
 
 const BasicLayout = ({ children }) => {
   return (
     <React.Fragment>
       <Header />
-      <div className="animate__animated animate__fadeIn">
-        {children}
-      </div>
+      <LazyLoad>
+        <div className="animate__animated animate__fadeIn">
+          {children}
+        </div>
+      </LazyLoad>
       <Footer />
     </React.Fragment>
   );
