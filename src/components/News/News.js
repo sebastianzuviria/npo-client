d4e4c1c1cf0994be8bee4fd60f9a62607b789f51
gabrielgagz <g.a.gomez@gmail.com>
2021-05-22 17:31:29 -0300

    NEWS: link details

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import apiGetService from '../../services/apiGetService';
 import './News.css';
+import { Link } from 'react-router-dom';
 
 const News = () => {
 
@@ -22,13 +23,17 @@ const News = () => {
                 news.slice(0, 3).map(({ id, createdAt, content, image, title }) => (
                     <div className='col my-3' key={id}>
                         <div className='card news__card h-100 border-0'>
-                            <img
-                                className='card-img-top news__img'
-                                src={image}
-                                alt={image}
-                            />
+                            <Link to={ `/news/${ id }` }>
+                                <img
+                                    className='card-img-top news__img'
+                                    src={image}
+                                    alt={image}
+                                />
+                            </Link>
                             <div className='card-body'>
-                                <p className='news__date mb-1 ms-1'>{ new Date( createdAt ).toLocaleDateString("es-ES") }</p>
+                                <p className='news__date mb-1 ms-1'>
+                                    { new Date( createdAt ).toLocaleDateString("es-ES", {weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' }) }
+                                </p>
                                 <h5 className='card-title news__title'>
                                     {title}
                                 </h5>
@@ -36,13 +41,13 @@ const News = () => {
                                 <p className='card-text text-muted text-justify'>
                                     { content.slice(0,230) } ...
                                 </p>
-                                {/* <button className='btn my-3 px-3 py-3 news__btn-info'>Leer Más</button> */}
                             </div>
                         </div>
                     </div>
                 ))}
         </div>
-        </div>
+        
+    </div>
     );
 }
 
