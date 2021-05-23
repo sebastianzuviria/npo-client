e2e7e809a5a3663a09804aa3d332dae6e4019afa
gabrielgagz <g.a.gomez@gmail.com>
2021-05-23 01:23:45 -0300

    NEWS: improve layout. Use brand color. Create 'MORE NEWS' button.

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 const News = () => {
 
     const [news, setNews] = useState([]);
+    const [loadedNews, setLoadedNews] = useState(3);
 
     const fetchNews = async () => {
         const data = await apiGetService('news');
@@ -14,15 +15,15 @@ const News = () => {
 
     useEffect(() => {
         fetchNews();
-    }, []);
+    }, [ loadedNews ]);
 
     return (
     <div className='container'>
         <div className='row row-cols-1 row-cols-md-3 my-4'>
             {news.length > 0 &&
-                news.slice(0, 3).map(({ id, createdAt, content, image, title }) => (
+                news.slice(0, loadedNews).map(({ id, createdAt, content, image, title }) => (
                     <div className='col my-3' key={id}>
-                        <div className='card news__card h-100 border-0'>
+                        <div className='card news__card h-100 border-0 shadow animate__animated animate__fadeIn'>
                             <Link to={ `/news/${ id }` }>
                                 <img
                                     className='card-img-top news__img'
@@ -31,13 +32,13 @@ const News = () => {
                                 />
                             </Link>
                             <div className='card-body'>
-                                <p className='news__date mb-1 ms-1'>
+                                <p className='news__date my-2 ms-1'>
                                     { new Date( createdAt ).toLocaleDateString("es-ES", {weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' }) }
                                 </p>
                                 <h5 className='card-title news__title'>
                                     {title}
                                 </h5>
-                                <hr />
+                                <hr className='news__hr'/>
                                 <p className='card-text text-muted text-justify'>
                                     { content.slice(0,230) } ...
                                 </p>
@@ -46,7 +47,12 @@ const News = () => {
                     </div>
                 ))}
         </div>
-        
+        <button 
+            className='btn news__btn-more mt-3 mb-5 p-3 mx-auto d-block'
+            onClick={ ()  => setLoadedNews( loadedNews + 3 ) }
+        >
+            VER MAS NOTICIAS
+        </button>
     </div>
     );
 }
