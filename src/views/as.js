02aac7fe5f654c5e8889e29b8d1f626cc3d9e7e7
Nataly Mora Mancera <tatamora21@gmail.com>
2021-05-22 19:09:23 -0500

    Adjusted design page about us

--- src/views/as.js
+++ src/views/as.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import MembersOrganization from '../components/Members/Members'
+import BasicLayout from '../Layouts/BasicLayout';
 
 
 
 const As = () => {
     return (
-        <div>
-            <h2>As</h2>
-            <MembersOrganization/>
-        </div>
-        
+        <>
+            <BasicLayout>
+                <main>
+                    <div class="py-5 text-center">
+                        <h2>As</h2>
+                        <p class="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+                    </div>
+                </main>
+                <div class="container marketing">
+                    <div class="row" >
+                        <MembersOrganization/>
+                    </div>
+                </div>
+            </BasicLayout>
+        </>
     )
 }
 
