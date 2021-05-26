a9dd05e3bd3640ad533f08ca201cf57835ce9f29
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-26 18:01:43 -0300

    Redesign of backoffice navigation bar

--- src/Layouts/BackOfficeLayout.js
+++ src/Layouts/BackOfficeLayout.js
@@ -5,12 +5,8 @@ import Sidebar from '../components/BackOffice/Sidebar/Sidebar';
 const BackOfficeLayout = ({ children }) => {
   return (
     <BasicLayout>
-      <div className="container-fluid">
-        <div className="row">
-          <Sidebar />
-          <div className="col-md-8 col-lg-9 col-xl-10">{children}</div>
-        </div>
-      </div>
+      <Sidebar />
+      <div className="row container-fluid">{children}</div>
     </BasicLayout>
   );
 };
