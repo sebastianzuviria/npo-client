4e245e442a0e851fd7cde1f33bf2910d49a2a5d2
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-30 19:17:09 -0300

    added compoenent to shot the  image of user

---
+++ src/components/Profile/UpdateImage/UpdateImage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import EditBtn from '../EditBtn/EditBtn';
+import ImageForm from './ImageForm';
+import './UpdateImage.css';
+
+const UpdateImage = ({ user }) => {
+  return (
+    <div className="d-flex flex-column justify-content-start align-items-center">
+      <div className="img-container rounded-circle border border-3">
+        {user.image ? (
+          <img
+            src={user.image}
+            className="img-fluid"
+            alt={`Imagen de perfil de ${user.firstName} ${user.lastName}`}
+          />
+        ) : (
+          `${user.firstName} ${user.lastName}`
+        )}
+      </div>
+      <EditBtn text="Editar imagen" icon="fa-cloud-upload">
+        <ImageForm />
+      </EditBtn>
+    </div>
+  );
+};
+
+export default UpdateImage;
