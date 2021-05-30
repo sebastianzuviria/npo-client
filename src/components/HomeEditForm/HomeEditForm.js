513ef2efe1739248bfcb4c84ebefa68c90ab95db
gabrielgagz <g.a.gomez@gmail.com>
2021-05-30 19:54:39 -0300

    HOME/EDIT: set welcomeText

--- src/components/HomeEditForm/HomeEditForm.js
+++ src/components/HomeEditForm/HomeEditForm.js
@@ -1,70 +1,105 @@
 import React, { useEffect, useState } from 'react';
-import EditSlides from './EditSlides/EditSlides';
+import { successAlert, errorAlert } from '../Alert/Alert';
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import apiGetService from '../../services/apiGetService';
+import apiUpdateService from '../../services/apiUpdateService';
+import InputField from '../SignupForm/InputField';
 import './HomeEditForm.css';
-import mockData from './mockEditForm.js';
-import { successAlert ,cancelAlert ,errorAlert, infoAlert, confirmAlert } from '../Alert/Alert';
 
-const HomeEditForm = (props) => {
+const HomeEditForm = () => {
     
-    const [currentData, setCurrentData] = useState(mockData);
-    const [isModeEdit, setIsModeEdit] = useState(false);
+    const [ { id, welcomeText }, setHomeState ] = useState({ id: '', welcomeText: '' });
+    
+    // Here we bring organization data from DB
+    useEffect(() => {
 
-    const handleOnChange = (e) => {
-        setCurrentData({
-            ...currentData,
-            [e.target.name]: e.target.value
-        })
+        (async () => {
 
-    }
-    const handleOnChangeSlides= (e, slide) => {
+            const { id, welcomeText } = await apiGetService('organizations/public');
+            setHomeState({
+                id,
+                welcomeText
+            });
+
+        })()
+
         
-        setCurrentData({
-            ...currentData,
-            slides: currentData.slides.map((value) => value.id === slide.id ? {...slide, [e.target.name]: e.target.value} : value)
-        })
+    }, []);
 
-    }
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const editHomeSchema = Yup.object().shape({
 
-        if(currentData.message.length < 20) return await infoAlert(`El texto de bienvenida debe contener al menos 20 caracteres`);
+        welcomeText: Yup.string()
+            .min(50, '¡El texo es demasiado corto!')
 
-        await successAlert()
+    });
+
+    const handleSubmit = async ( values ) => {
+
+        try {
+        
+            await apiUpdateService(`organizations`, id,  values );
+            await successAlert();
+
+        } catch (err) {
+
+            await errorAlert();
+
+        }
         
     }
-      
+    
     return(
-        <form className="HomeEditForm__content" onSubmit={handleSubmit}>
+        <div className='container mt-4'>
+            <h3>Editar Página de Inicio</h3>
+            <Formik
+                enableReinitialize={ true }
+                initialValues={{ welcomeText }}
+                validationSchema={ editHomeSchema }
+                onSubmit={ handleSubmit }
+            >
+                <Form>
+                    <div className='mb-3'>
+                        <InputField label='Texto de Bienvenida' type='text' className='form-control' name='welcomeText' />
+                    </div>
+                    <button className='btn HomeEditForm__btn' type='submit'>
+                        Actualizar
+                    </button>
+                </Form>
+            </Formik>
+        </div>
+        
+        // <form className='HomeEditForm__content' onSubmit={handleSubmit}>
 
-            <div className="HomeEditForm__content-input">
+        //     <div className='HomeEditForm__content-input'>
 
-                <label className="HomeEditForm__label-title">título de portada:
-                    <input type="text" name="title" className="HomeEditForm__input-title" autoComplete="off" 
-                        value={currentData.title} 
-                        onChange={handleOnChange}
-                    />
-                </label>
-            </div>
+        //         <label className='HomeEditForm__label-title'>título de portada:
+        //             <input type='text' name='title' className='HomeEditForm__input-title' autoComplete='off' 
+        //                 value={currentData.title} 
+        //                 onChange={handleOnChange}
+        //             />
+        //         </label>
+        //     </div>
 
-            <div className="HomeEditForm__content-input">
+        //     <div className='HomeEditForm__content-input'>
 
-                <label className="HomeEditForm__label-message">Texto de bienvenida:
-                    <textarea name="message" className="HomeEditForm__input-message" autoComplete="off" 
-                        value={currentData.message} 
-                        onChange={handleOnChange}
-                    ></textarea>
-                </label>
-            </div>
+        //         <label className='HomeEditForm__label-message'>Texto de bienvenida:
+        //             <textarea name='message' className='HomeEditForm__input-message' autoComplete='off' 
+        //                 value={currentData.message} 
+        //                 onChange={handleOnChange}
+        //             ></textarea>
+        //         </label>
+        //     </div>
 
-            <div className="HomeEditForm__content-slides">
+        //     <div className='HomeEditForm__content-slides'>
                 
-                {
-                    currentData.slides?.map((value) => <EditSlides key={value.id} {...value} handleOnChangeSlides={(e) => handleOnChangeSlides(e,value)} />)
-                }
+        //         {
+        //             currentData.slides?.map((value) => <EditSlides key={value.id} {...value} handleOnChangeSlides={(e) => handleOnChangeSlides(e,value)} />)
+        //         }
 
-            </div>
-            <button className="HomeEditForm__button" type="submit">Guardar Cambios</button>
-        </form>
+        //     </div>
+        //     <button className='HomeEditForm__button' type='submit'>Guardar Cambios</button>
+        // </form>
     );
 }
 
--- src/components/WelcomeText/WelcomeText.js
+++ src/components/WelcomeText/WelcomeText.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import apiGetService from '../../services/apiGetService';
 import './WelcomeText.css';
 
 const WelcomeText = () => {
+
+  const [ welcomeTextState, setWelcomeTexState ] = useState( 'Cargando Texto de Bienvenida...' );
+    
+    // Here we bring organization data from DB
+    useEffect(() => {
+
+        (async () => {
+
+            const { welcomeText } = await apiGetService('organizations/public');
+            setWelcomeTexState(welcomeText);
+
+        })()
+
+        
+    }, []);
 
   return (
 
@@ -10,7 +26,7 @@ const WelcomeText = () => {
         ¡Bienvenid@ a nuestra página!
       </h1>
       <p className="lead mt-4 welcomeText__text">
-        Lorem ipsum dolor sit amet, consectetur <strong>adipiscing</strong> elit. Vestibulum mattis libero non libero luctus feugiat. Aliquam erat volutpat. Integer et dapibus nibh, at aliquam urna. Fusce faucibus lacus sit amet <strong>tellus</strong> bibendum elementum. Phasellus elit nunc, ornare nec <strong>ultrices</strong> sit amet, imperdiet at eros. Praesent sem nisl, euismod vel urna ut, mollis gravida est. <strong>Etiam</strong> ut velit at leo venenatis.
+        {welcomeTextState}
       </p>
     </div>
   
