46b289c153b18e97605e014d7d3aae44fd844dff
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-06-01 16:18:07 -0300

    alers message and style of backoffice

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -4,57 +4,41 @@ import Swal from 'sweetalert2';
 const MySwal = withReactContent(Swal);
 
 export const confirmAlert = () => {
-
-    return MySwal.fire({
-        icon: 'warning',
-        title: '¿Estas seguro?',
-        showDenyButton: true,
-        confirmButtonText: 'SI',
-        denyButtonText: 'NO'
-      })
-
-}
+  return MySwal.fire({
+    icon: 'warning',
+    title: '¿Estas seguro?',
+    showDenyButton: true,
+    confirmButtonText: 'SI',
+    denyButtonText: 'NO'
+  });
+};
 
 export const successAlert = () => {
-
-    return MySwal.fire({
-
-        icon: 'success',
-        title: 'Todo correcto!!',
-
-        showConfirmButton: false,
-        timer: 2000
-      })
-
-}
+  return MySwal.fire({
+    icon: 'success',
+    showConfirmButton: false,
+    timer: 2000
+  });
+};
 
 export const cancelAlert = () => {
-    return MySwal.fire({
-        title: 'Cancelado',
-        icon: 'error',
-        showConfirmButton: false,
-        timer: 2000
-    })
-}
+  return MySwal.fire({
+    title: 'Cancelado',
+    icon: 'error',
+    showConfirmButton: false,
+    timer: 2000
+  });
+};
 
 export const errorAlert = () => {
-    
-    return MySwal.fire({
-
-        title: 'Error',
-        icon: 'error',
-        showConfirmButton: false,
-        timer: 2000
-    })
-
-}
+  return MySwal.fire({
+    title: 'Error, algo salió mal ',
+    icon: 'error',
+    showConfirmButton: false,
+    timer: 2000
+  });
+};
 
 export const infoAlert = (text) => {
-
-    return MySwal.fire({icon: 'info', text})
-
-}
-
-
-
-
+  return MySwal.fire({ icon: 'info', text });
+};
