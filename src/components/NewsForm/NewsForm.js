f34bc26bfda6cd39d0ed3f6577e3c5fa54fac8d3
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-06-01 14:59:20 -0300

    fix userslist and newsForm styles

--- src/components/NewsForm/NewsForm.js
+++ src/components/NewsForm/NewsForm.js
@@ -6,149 +6,165 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import apiPostService from '../../services/apiPostService';
 import apiGetService from '../../services/apiGetService';
 import apiUpdateService from '../../services/apiUpdateService';
-import { loadNovelties, updateNovelties } from '../../slices/backNoveltiesSlice'
+import {
+  loadNovelties,
+  updateNovelties
+} from '../../slices/backNoveltiesSlice';
 
 const NewsForm = ({ id }) => {
-    const [allCategories, setAllCategories] = useState([])
-    const [newTitle, setNewTitle] = useState('');
-    const [newImage, setNewImage] = useState('');
-    const [imageUrl, setImageUrl] = useState('');
-    const [newContent, setNewContent] = useState('');
-    const [newCategoryId, setNewCategoryId] = useState('');
-    const [newCategoryName, setNewCategoryName] = useState('')
-    const [isEdit, setIsEdit] = useState(false);
-    const [titleOfForm, setTitleOfForm] = useState('Crear Novedad');  
-    const dispatch = useDispatch()
+  const [allCategories, setAllCategories] = useState([]);
+  const [newTitle, setNewTitle] = useState('');
+  const [newImage, setNewImage] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [newContent, setNewContent] = useState('');
+  const [newCategoryId, setNewCategoryId] = useState('');
+  const [newCategoryName, setNewCategoryName] = useState('');
+  const [isEdit, setIsEdit] = useState(false);
+  const [titleOfForm, setTitleOfForm] = useState('Crear Novedad');
+  const dispatch = useDispatch();
 
-    console.log(newCategoryId)
+  useEffect(() => {
+    (async () => {
+      const categories = await apiGetService('categories');
+      setAllCategories(categories);
+    })();
+  }, []);
 
-    useEffect(() => {
-        (async () => {
-            const categories = await apiGetService('categories')
-            setAllCategories(categories)
-        })()
-    }, [])
+  useEffect(() => {
+    if (id) {
+      (async () => {
+        const categories = await apiGetService('categories');
+        setAllCategories(categories);
+        const novelty = await apiGetService('news', String(id));
+        console.log(novelty);
+        setIsEdit(true);
+        setNewTitle(novelty.title);
+        setImageUrl(novelty.image);
+        setNewCategoryId(novelty.category.id);
+        setNewCategoryName(novelty.category.name);
+        setNewContent(novelty.content);
+        setTitleOfForm('Editar Novedad');
+      })();
+    }
+  }, [id]);
 
-    useEffect(() => {
-        if (id) {
-            (async () => {
-                const categories = await apiGetService('categories')
-                setAllCategories(categories)
-                const novelty = await apiGetService('news', String(id))
-                console.log(novelty)
-                setIsEdit(true) 
-                setNewTitle(novelty.title)
-                setImageUrl(novelty.image)
-                setNewCategoryId(novelty.category.id)
-                setNewCategoryName(novelty.category.name)
-                setNewContent(novelty.content)
-                setTitleOfForm('Editar Novedad')
-            })()
-        }
-    },[id]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const formData = new FormData()
-        formData.append('image', newImage)
-        formData.append('title', newTitle)
-        formData.append('category', newCategoryId)
-        formData.append('content', newContent) 
-        formData.append('imageUrl', imageUrl)
-        const config = {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        }       
-
-        try {
-            if (isEdit) {
-                const response = await apiUpdateService('news',id, formData, config)
-                dispatch(updateNovelties(response))
-                console.log(response)
-                setNewTitle('')
-                setImageUrl('')
-                setNewCategoryId('')
-                setNewCategoryName('')
-                setNewContent('')
-            } else {
-                const response = await apiPostService('news', formData, config)
-                dispatch(loadNovelties())
-                console.log(response)
-                setNewTitle('')
-                setImageUrl('')
-                setNewCategoryId('')
-                setNewCategoryName('')
-                setNewContent('')
-            }
-        } catch (error) {
-            console.log(error);
-        }
+    const formData = new FormData();
+    formData.append('image', newImage);
+    formData.append('title', newTitle);
+    formData.append('category', newCategoryId);
+    formData.append('content', newContent);
+    formData.append('imageUrl', imageUrl);
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
     };
 
-    console.log(newContent)
+    try {
+      if (isEdit) {
+        const response = await apiUpdateService('news', id, formData, config);
+        dispatch(updateNovelties(response));
+        setNewTitle('');
+        setImageUrl('');
+        setNewCategoryId('');
+        setNewCategoryName('');
+        setNewContent('');
+      } else {
+        const response = await apiPostService('news', formData, config);
+        dispatch(loadNovelties());
+        setNewTitle('');
+        setImageUrl('');
+        setNewCategoryId('');
+        setNewCategoryName('');
+        setNewContent('');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-        <div className='d-flex justify-content-center form-container'>
-            <form className='form-new' onSubmit={handleSubmit}>
-            <h2 className='text-center'>{titleOfForm}</h2>
-                <label className='w-100'>
-                    Titulo
-                    <input 
-                        className='input-new'
-                        type='text'
-                        value={newTitle}
-                        onChange={({target}) => { setNewTitle(target.value) }}
-                    />
-                </label>
-                <label className='w-100 '>
-                    <div>Imagen</div>
-                    <div className='image-container'>
-                    {id && imageUrl !== '' && <img style={{ width: '30%'}} src={imageUrl} alt='prueba'/>}
-                    <input 
-                        className='input-image'
-                        type='file'
-                        onChange={({target}) => { setNewImage(target.files[0]) }}
-                    />
-                    </div>
-                </label>
-                <label className='w-100'>
-                    Categoria
-                    <select
-                        className='select-new'
-                        value={newCategoryId}
-                        onChange={({ target }) => setNewCategoryId(target.value)}
-                    >
-                        {id ? (<option value={newCategoryId} defaultValue>{newCategoryName}</option>) : <option value='' defaultValue>Select</option>}
-                        
-                        {allCategories.map(c => {
-                            if(newCategoryId !== c.id) {
-                               return <option key={c.name} value={c.id}>{c.name}</option>
-                            } else {
-                                return null
-                            }
-                        }
-                        )}
-                    </select>
-                </label>
-                <label className='content-new'>
-                    Contenido
-                    <CKEditor
-                        editor={ ClassicEditor }
-                        data={newContent}
-                        onChange={ ( event, editor ) => {
-                            const data = editor.getData();
-                            setNewContent(data)
-                        } }
-                    />
-                </label>
-                <div className='d-flex justify-content-center'>
-                    <button className='button-new' type='submit'>{isEdit ? 'Edit' : 'Create'}</button>
-                </div>
-            </form>
+  return (
+    <div className="d-flex justify-content-center form-container">
+      <form className="form-new w-100" onSubmit={handleSubmit}>
+        <h2 className="text-center">{titleOfForm}</h2>
+        <label className="w-100">
+          Titulo
+          <input
+            className="input-new"
+            type="text"
+            value={newTitle}
+            onChange={({ target }) => {
+              setNewTitle(target.value);
+            }}
+          />
+        </label>
+        <label className="w-100 ">
+          <div>Imagen</div>
+          <div className="image-container">
+            {id && imageUrl !== '' && (
+              <img style={{ width: '30%' }} src={imageUrl} alt="prueba" />
+            )}
+            <input
+              className="input-image"
+              type="file"
+              onChange={({ target }) => {
+                setNewImage(target.files[0]);
+              }}
+            />
+          </div>
+        </label>
+        <label className="w-100">
+          Categoria
+          <select
+            className="select-new"
+            value={newCategoryId}
+            onChange={({ target }) => setNewCategoryId(target.value)}
+          >
+            {id ? (
+              <option value={newCategoryId} defaultValue>
+                {newCategoryName}
+              </option>
+            ) : (
+              <option value="" defaultValue>
+                Select
+              </option>
+            )}
+
+            {allCategories.map((c) => {
+              if (newCategoryId !== c.id) {
+                return (
+                  <option key={c.name} value={c.id}>
+                    {c.name}
+                  </option>
+                );
+              } else {
+                return null;
+              }
+            })}
+          </select>
+        </label>
+        <label className="content-new">
+          Contenido
+          <CKEditor
+            editor={ClassicEditor}
+            data={newContent}
+            onChange={(event, editor) => {
+              const data = editor.getData();
+              setNewContent(data);
+            }}
+          />
+        </label>
+        <div className="d-flex justify-content-center">
+          <button className="button-new" type="submit">
+            {isEdit ? 'Edit' : 'Create'}
+          </button>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default NewsForm;
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,60 +1,61 @@
-import React, { useState, useEffect } from 'react'
-import './Table.css'
-import apiGetService from '../../services/apiGetService'
-import apiDeleteService from '../../services/apiDeleteService'
-import apiUpdateService from '../../services/apiUpdateService'
+import React, { useState, useEffect } from 'react';
+import './Table.css';
+import apiGetService from '../../services/apiGetService';
+import apiDeleteService from '../../services/apiDeleteService';
+import apiUpdateService from '../../services/apiUpdateService';
 import { successAlert, cancelAlert, confirmAlert } from '../Alert/Alert';
-import { Button, Modal } from 'react-bootstrap'
+import { Button, Modal } from 'react-bootstrap';
 
 const Table = () => {
-  const [users, setUsers] = useState([])
-  const [newObject, setNewObject] = useState({})
+  const [users, setUsers] = useState([]);
+  const [newObject, setNewObject] = useState({});
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  
+
   const update = async (id) => {
+    console.log(newObject);
     const res = await confirmAlert();
     if (res.isConfirmed) {
-      await apiUpdateService('users', id, newObject)
-      setUsers(users.map(user => (user.id === newObject.id ? newObject : user)))
-      handleClose()
-      return successAlert()
+      await apiUpdateService('users', id, newObject);
+      setUsers(
+        users.map((user) => (user.id === id ? { ...user, ...newObject } : user))
+      );
+      handleClose();
+      return successAlert();
     } else {
       cancelAlert();
     }
-  }
+  };
   const delet = async (type, id) => {
     const res = await confirmAlert();
     if (!res.isConfirmed) {
       return await cancelAlert();
     }
-    const deleted = await apiDeleteService(type, id)
+    const deleted = await apiDeleteService(type, id);
     if (deleted) {
-      let newUser = users.filter(user => {
-        return user.id !== id
-      })
-      setUsers(newUser)
-      return successAlert()
+      let newUser = users.filter((user) => {
+        return user.id !== id;
+      });
+      setUsers(newUser);
+      return successAlert();
     }
   };
   useEffect(() => {
-
     (async () => {
       const returnedUsers = await apiGetService('users');
       setUsers(returnedUsers);
     })();
-
-  }, [])
+  }, []);
   return (
-    <div className='d-flex justify-content-center'>
+    <div className="d-flex justify-content-center">
       <table className="table table-bordered table-hw">
         <thead>
           <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Actions</th>
+            <th scope="col">Nombre/s</th>
+            <th scope="col">Apellido/s</th>
+            <th scope="col">Rol</th>
+            <th scope="col">Acciones</th>
           </tr>
         </thead>
         <tbody>
@@ -62,31 +63,39 @@ const Table = () => {
             <tr key={i}>
               <td>{user.firstName}</td>
               <td>{user.lastName}</td>
-              <td>{user.email}</td>
+              <td>{user.roleId}</td>
               <td>
-                <Button variant="primary" onClick={/* () => edit('users',user.id)*/handleShow}>
+                <Button
+                  variant="primary"
+                  onClick={/* () => edit('users',user.id)*/ handleShow}
+                >
                   <i className="fa fa-pencil" aria-hidden="true"></i>
                 </Button>
                 <Modal show={show} onHide={handleClose} animation={false}>
                   <Modal.Header closeButton>
-                    <Modal.Title>Edit Role</Modal.Title>
+                    <Modal.Title>Editar Rol</Modal.Title>
                   </Modal.Header>
                   <Modal.Body>
                     <label className="form-label">Role ID</label>
-                    <select className='form-control' 
-                      name='roleId' 
+                    <select
+                      className="form-control"
+                      defaultValue={user.roleId}
+                      name="roleId"
                       onChange={(e) => {
-                        setNewObject({ ...newObject, [e.target.name]: e.target.value })
-                      }}>
-                      <option value={1}>1</option>
-                      <option value={2}>2</option>
+                        setNewObject({
+                          [e.target.name]: e.target.value
+                        });
+                      }}
+                    >
+                      <option value={1}>1 - Admin</option>
+                      <option value={2}>2 - Standard</option>
                     </select>
                   </Modal.Body>
                   <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                       Close
                     </Button>
-                    <Button variant="primary" onClick={()=>update(user.id)}>
+                    <Button variant="primary" onClick={() => update(user.id)}>
                       Save Changes
                     </Button>
                   </Modal.Footer>
@@ -103,7 +112,7 @@ const Table = () => {
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
