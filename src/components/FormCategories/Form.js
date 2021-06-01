46b22e37054d84eab496c8903674ac24eba89896
Nataly Mora Mancera <tatamora21@gmail.com>
2021-06-01 02:09:14 -0500

    Adjusted create category form

--- src/components/FormCategories/Form.js
+++ src/components/FormCategories/Form.js
@@ -1,69 +1,87 @@
 import React,{useState,useEffect} from 'react'
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import apiPostService from '../../services/apiPostService';
+import { Modal, Button } from 'react-bootstrap'
+
+import {
+    successAlert,
+    cancelAlert,
+    confirmAlert,
+    errorAlert,
+  } from "../Alert/Alert";
+
 import './form.css';
 
-const Form = ({category}) => {
+const Form = ({setCategories}) => {
     const [name, setName] = useState('');
     const [description, setdescription] = useState('');
-    console.log(category);
-
-    useEffect(() => {
-        if (category) {
-            setName(category.name)
-            setdescription(category.description)
-        }
-    },[]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const handleSubmit= async (e)=>{
+
         e.preventDefault();
         const objcategory = {
             name,
             description
         }
         console.log(objcategory);
-        /* try {
-            if (category) {
-                //const editedcategory = await categoryService.editcategory(id, objcategory);
-                console.log(category, 'edited category');
-            } else {
-                //const createdcategory = await categoryService.createcategory(objcategory);
-                console.log(category, 'created category');
-            }
+        try {
+            const createdcategory = await apiPostService('categories', objcategory);  
+            setName('')
+            setdescription('')
+            setIsModalOpen('')
+            setIsModalOpen(false);    
+            await successAlert(); 
+
         } catch (error) {
-            console.log(error);
-        } */
+            setIsModalOpen(false);     
+            errorAlert();
+        }
     }
     return (
-        
-        <div className='form-Categories'>
-            <h2 className='text-center'>{(category)?'Edit category':'Create category'}</h2>
-            <form
-                onSubmit={handleSubmit}>
-                    <label className='description-cat'>
-                        Name
-                        <input 
-                            className='input-cat'
-                            type="text"
-                            placeholder="Name" 
-                            value={(category)?name:''}
-                            onChange={(e)=>{setName(e.target.value)}}
-                        />
-                    </label>
-                    <label className='description-cat'>
-                        Description
-                        <CKEditor
-                            editor={ ClassicEditor }
-                            data={(category)?description:''}
-                            onChange={ ( event, editor ) => {
-                                const data = editor.getData();
-                                setdescription(data)
-                            } }
-                        />
-                    </label>
-                    <button className='button-category'>{(category?'Update':'Create')}</button>
-            </form>
+        <>
+        <div style={{ display: 'flex', justifyContent: 'center'}}>
+            <Button style={{ margin: '30px', backgroundColor: '#0dcaf0', border: 'none'}} onClick={()=> setIsModalOpen(!isModalOpen)}>Crear categría</Button>
         </div>
+
+        <Modal size='lg' show={isModalOpen} onHide={() => setIsModalOpen(!isModalOpen)} animation={false}>
+            <Modal.Header closeButton>
+                <Modal.Title></Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className='form-Categories'>
+                    <h2 className='text-center'>Create categoría</h2>
+                    <form
+                        onSubmit={handleSubmit}>
+                            <label className='description-cat'>
+                                Name
+                                <input 
+                                    className='input-cat'
+                                    type="text"
+                                    placeholder="Name" 
+                                    value={name}
+                                    onChange={(e)=>{setName(e.target.value)}}
+                                />
+                            </label>
+                            <label className='description-cat'>
+                                Description
+                                <CKEditor
+                                    editor={ ClassicEditor }
+                                    data={description}
+                                    onChange={ ( event, editor ) => {
+                                        const data = editor.getData();
+                                        setdescription(data)
+                                    } }
+                                />
+                            </label>
+                            <button className='button-category'>Crear categoría</button>
+                    </form>
+                </div>
+
+            </Modal.Body>
+        </Modal>
+    </>
     )
 }
 
